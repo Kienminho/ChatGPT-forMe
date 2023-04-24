a0ab4d50e31183830fbf9f70c88e3211d8d1ad55
Kienminho <85487815+Kienminho@users.noreply.github.com>
2023-04-24 19:50:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 //open ai
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = "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";
 const configuration = new Configuration({ apiKey: OPENAI_API_KEY });
 const openAI = new OpenAIApi(configuration);
 app.engine(
